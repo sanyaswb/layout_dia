55c019598605e7b5804b78e14a7224432ff393df
Oleksandr Merkulov <odin@terradeus.org>
2025-12-19 14:12:22 +0200

    Updates

--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import "./blocks/smart-loader.js";
-import "./blocks/division.js";
-import "./blocks/menu.js";
-import "./blocks/nav-bar.js";
-import "./blocks/parallax-scroll.js";
-import "./blocks/slider.js";
-import "./blocks/theme.js";
